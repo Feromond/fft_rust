43f1d8aece80be035f864faabf90554cd399b36e
Feromond <Jacobpmish@gmail.com>
2023-04-21 00:05:10 -0600

    tested some further implementation with streamlit

--- FileLoader.py
+++ FileLoader.py
@@ -5,19 +5,25 @@ import subprocess
 
 st.title("FFT Rust Data Loading")
 file_loader = st.empty()
-butn = st.empty()
 
 
 file = file_loader.file_uploader("Upload Data")
 
+
 if file != None:
-    df = pd.read_csv(file)
+    df = pd.read_csv(file, header=None)
+    st.dataframe(df)
+    butn = st.button("Press To Run", disabled=False, key=2)
+elif file == None:
+    st.stop()
 
-
-butn.button("Press To Run")
 
 if butn:
-    subprocess.call("cargo run")
+    subprocess.call("./fft_rust")
+
+    st.image("raw_signal.png")
+    st.divider()
+    st.image("fft_plot.png")
 
 
 
