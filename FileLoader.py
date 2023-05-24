6e22972de8228c488775f1a1a2d24737dd7cd7e5
Feromond <Jacobpmish@gmail.com>
2023-05-24 10:48:21 -0600

    organized paths and cleaned up

--- FileLoader.py
+++ FileLoader.py
@@ -1,22 +1,18 @@
 import streamlit as st
 import pandas as pd
 import subprocess
-
-
+import os
 
 def check_file(file):
     # Check if the file extension is valid
     if file.type not in ["text/csv", "text/plain"]:
         return False, "File type not supported. Please upload a .txt or .csv file."
     
-    # Check if the file has exactly two columns
+    # Load the file into a DataFrame
     df = pd.read_csv(file, header=None)
-    # if df.shape[1] != 2:
-    #     return False, "Invalid file format. Please upload a file with exactly two columns."
     
     # If everything checks out
     return True, df
-
 
 st.set_page_config(page_title="FFT Rust - File Loader", layout="wide")
 st.title("FFT Rust Data Loading")
@@ -24,10 +20,14 @@ file_loader = st.empty()
 
 file = file_loader.file_uploader("Upload Data")
 
+uploaded_file_path = None
 if file is not None:
     is_valid, result = check_file(file)
     if is_valid:
-        df = result
+        # Write the DataFrame to a file in the temporary_uploads directory
+        os.makedirs('temporary_uploads', exist_ok=True)
+        uploaded_file_path = f'temporary_uploads/{file.name}'
+        result.to_csv(uploaded_file_path, index=False, header=False)
         butn = st.button("Press To Run", disabled=False, key=2)
     else:
         st.error(result)
@@ -38,7 +38,9 @@ else:
 st.divider()
 
 if 'butn' in locals() and butn:
-    subprocess.call(["./target/release/fft_rust", f"./{file.name}"])
+    st.write(f"./{uploaded_file_path}")
+    subprocess.call(["./target/release/fft_rust", f"./{uploaded_file_path}"])
     col1, col2 = st.columns([1,1])
-    col1.image("raw_signal.png")
-    col2.image("amplitude_spectrum.png")
+    col1.image("output_figures/raw_signal.png")
+    col2.image("output_figures/amplitude_spectrum.png")
+    os.remove(f"./{uploaded_file_path}")
